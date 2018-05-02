6b3a26b0b4b80543cc98b4afc17133908b47c5ec
haniepong <haniepong@gmail.com>
2018-05-02 23:05:31 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import io
 import os
+import csv
 
 from nltk.parse import stanford
 from pydtk.tree import Tree
@@ -9,7 +10,7 @@ from pydtk.operation import fast_shuffled_convolution
 
 if __name__ == '__main__':
     filename = io.open("D:\dataset\essay01.ann", "r", encoding="utf-8")  # datasetnya
-    f2 = open("D:\dataset\hasilcoba2.txt", "a")  # hasil
+    f2 = open("D:\Stanford\TA\hasil.csv", "a")  # hasil
 
     paragraph = filename.read()
     filename.close()
@@ -27,45 +28,16 @@ if __name__ == '__main__':
         data3 = data2.replace("(", "")
         data4 = data3.replace(")", "")
         try:
-            parser = stanford.StanfordParser(
-                model_path='C:/Stanford/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz')
+            parser = stanford.StanfordParser(model_path='C:/Stanford/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz')
             sentences = list(parser.raw_parse(str(data4)))
-            tree = sentences[0]
-            fix = str(tree.productions)
+            fix = str(sentences)
             f2.write(fix + "\n")
         except:
             f2.write("KEONG\n")
-        #print (lists[i])
-        #print (tree.productions())
-        #print tree.pretty_print()
+            #print (lists[i])
+            #print (tree.productions())
+            #print tree.pretty_print()
 
     f2.close()
-
-    print (str(sentences[0]))
-
-    s1 = (str(sentences[0]))
-    s2 = ("(@S)")
-
-    t1 = Tree(string=s1)
-    t2 = Tree(string=s2)
-    #print (t1)
-    #print (t2)
-
-    dtCalculator = DT(dimension=8192, LAMBDA=0.6, operation=fast_shuffled_convolution)
-
-    distributedTree = dtCalculator.dt(t1)
-
-    print (distributedTree)
-
-    tk = TreeKernel()
-
-    dtk = DT(dimension=8192)
-    for tt in t1.allNodes():
-        print(tt.root, tk.evaluate(tt, tt), dtk.kernel(tt, tt))
-
-    print(tk.evaluate(t1, t2))
-
-    print(dtk.kernel(t1, t2))
-
 
 
